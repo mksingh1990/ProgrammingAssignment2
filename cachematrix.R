bc3f7b0ca9035029fb5e806c19d461ad227e741d
mksingh1990 <mksingh1990@gmail.com>
2015-10-25 09:13:22 +0530

    submission
    submission for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix function converts matrix into cache format , we need to pass square matrix to the function
+## After makeCacheMatrix , cacheSolve function gives inverse of function from cache if exists , else computes inverse
 
-## Write a short comment describing this function
+
+
+# we need to pass a square matrix into the function
+#z <- matrix(1:9,nrow=2,ncol=2,byrow=T)
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) inverse <<- solve
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
 
+## The below function returns invrese 
+
+cacheSolve <- function(x) {
+## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  } 
+  data <- x$get()
+  inverse <- solve(data)
+  x$setinverse(inverse)
+  inverse
+  
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#x <- makeCacheMatrix(z)
+#cacheSolve(x)
